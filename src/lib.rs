9c91bfd867e4ff0420b922de183a46361950948e
Scott Moore <scott.will.moore@gmail.com>
2018-03-15 18:23:01 +1100

    fix upper bound by making it inclusive

--- src/lib.rs
+++ src/lib.rs
@@ -10,20 +10,20 @@ impl Iterator for Prime {
     fn next(&mut self) -> Option<usize> {
         let current_prime = self.current_prime;
         let mut prime_multiple = current_prime * 2;
-        while prime_multiple < self.upper_bound {
+        while prime_multiple <= self.upper_bound {
             self.is_composite[prime_multiple] = true;
             prime_multiple += current_prime;
         }
 
         let mut next_prime = current_prime + 1;
-        while next_prime < self.upper_bound {
+        while next_prime <= self.upper_bound {
             if !self.is_composite[next_prime] {
                 break;
             }
             next_prime += 1;
         }
 
-        if current_prime < self.upper_bound {
+        if current_prime <= self.upper_bound {
             self.current_prime = next_prime;
             Some(current_prime)
         } else {
@@ -34,7 +34,7 @@ impl Iterator for Prime {
 
 pub fn generate(upper_bound: usize) -> Prime {
     Prime {
-        is_composite: vec![false; upper_bound],
+        is_composite: vec![false; upper_bound + 1],
         current_prime: 2,
         upper_bound,
     }
