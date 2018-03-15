4c9dda73bfd354096cbd9bfc01b4dbdff70b59fc
Scott Moore <scott.will.moore@gmail.com>
2018-03-15 17:49:59 +1100

    rewrite sieve algorithm in procedural style

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,43 @@
 pub struct Prime {
-    sieve: Vec<bool>,
-    current: usize,
+    is_composite: Vec<bool>,
+    current_prime: usize,
+    upper_bound: usize,
 }
 
 impl Iterator for Prime {
     type Item = usize;
 
     fn next(&mut self) -> Option<usize> {
-        let prime = self.current + 2;
+        let current_prime = self.current_prime;
+        let mut prime_multiple = current_prime * 2;
+        while prime_multiple < self.upper_bound {
+            self.is_composite[prime_multiple] = true;
+            prime_multiple += current_prime;
+        }
 
-        let sieve: Vec<bool> = self.sieve
-            .iter()
-            .enumerate()
-            .map(|(index, &value)| {
-                if value {
-                    (index + 2) % prime != 0
-                } else {
-                    false
-                }
-            })
-            .collect();
-        self.sieve = sieve;
+        let mut next_prime = current_prime + 1;
+        while next_prime < self.upper_bound {
+            if !self.is_composite[next_prime] {
+                break;
+            }
+            next_prime += 1;
+        }
 
-        println!("{:#?}", &self.sieve);
-
-        let (next, _) = self.sieve
-            .iter()
-            .enumerate()
-            .find(|&(_, &value)| value == true)
-            .unwrap();
-        self.current = next;
-
-        if next * next > self.sieve.len() * self.sieve.len() {
-            None
+        if current_prime < self.upper_bound {
+            self.current_prime = next_prime;
+            Some(current_prime)
         } else {
-            Some(prime)
+            None
         }
     }
 }
 
-pub fn generate(size: usize) -> Prime {
-    let sieve = vec![true; size];
+pub fn generate(upper_bound: usize) -> Prime {
+    assert!(upper_bound > 0);
 
-    Prime { sieve, current: 0 }
+    Prime {
+        is_composite: vec![false; upper_bound],
+        current_prime: 2,
+        upper_bound,
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 extern crate prime_sieve;
 
 fn main() {
-    for prime in prime_sieve::generate(15).take(5) {
-        println!("{}", prime);
+    for prime in prime_sieve::generate(100) {
+        print!("{}, ", prime);
     }
+    println!();
 }
